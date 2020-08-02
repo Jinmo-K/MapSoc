133e33d2b1d9292be3f800ff8f1f5e0d9582cddf
Jinmo <jinmocoding@gmail.com>
2020-08-02 09:40:23 -0700

    Added silencing of logs in production

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,6 +6,15 @@ import { store } from './store';
 import App from './App';
 import './index.css';
 
+// Silence logging in production
+if(process.env.NODE_ENV === 'production'){
+    if(!window.console) window.console = {};
+    var methods = ["log", "debug", "warn", "info"];
+    for(let method of methods){
+        console[method] = () => {};
+    }
+}
+
 render(
   <Provider store={store}>
     <App />
